fa36cab6ad459a8ac5cce4587746e04ed24c428c
johnfrancis111 <nnopuechichijioke@gmail.com>
2026-06-01 23:56:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Create variables
+let a = 10;
+let b = 5;
+
+// Add numbers
+let sum = a + b;
+console.log("Sum:", sum);
+
+// Compare values
+console.log("a > b:", a > b);
+console.log("a == b:", a == b);
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 4));
+function greet (name) {
+    return "Hello"
+}
+
+console.log(greet("chinelo"));
